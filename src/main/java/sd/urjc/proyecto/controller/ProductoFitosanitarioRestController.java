87390d7a2aca2421ba43eb2500ebee60b7123f5e
Milagros Mouriño <milagros2706m@gmail.com>
2021-05-05 17:30:23 +0200

    Add get productos and plagas

--- src/main/java/sd/urjc/proyecto/controller/ProductoFitosanitarioRestController.java
+++ src/main/java/sd/urjc/proyecto/controller/ProductoFitosanitarioRestController.java
@@ -1,5 +1,6 @@
 package sd.urjc.proyecto.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,11 @@ public class ProductoFitosanitarioRestController {
 
 	@Autowired
 	private ProductoFitosanitarioRepository repProductos;
+	
+	@GetMapping("/")
+    private ResponseEntity<List<ProductoFitosanitario>> getProductos() {
+        return new ResponseEntity<List<ProductoFitosanitario>>(repProductos.findAll(), HttpStatus.OK);
+    }
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<ProductoFitosanitario> getProducto(@PathVariable String id){
