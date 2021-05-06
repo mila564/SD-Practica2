2b1d564edc2ba4c514fdf07827609c29ae7ffe51
Sonia <scasero00@gmail.com>
2021-05-06 10:59:24 +0200

    post Construct de categoria

--- src/main/java/sd/urjc/proyecto/controller/CategoriaCultivoController.java
+++ src/main/java/sd/urjc/proyecto/controller/CategoriaCultivoController.java
@@ -1,7 +1,10 @@
 package sd.urjc.proyecto.controller;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,12 +16,38 @@ import org.springframework.web.bind.annotation.RestController;
 
 import sd.urjc.proyecto.model.CategoriaCultivo;
 import sd.urjc.proyecto.repository.CategoriaCultivoRepository;
+import sd.urjc.proyecto.repository.EspecieRepository;
 
 @RequestMapping("/categoriaCultivo")
 @RestController
 public class CategoriaCultivoController {
 	@Autowired
 	private CategoriaCultivoRepository repCategoriaCultivo;
+	
+	@Autowired
+	private EspecieRepository repEspecie;
+	
+	@PostConstruct
+	public void init() {
+		repCategoriaCultivo.save(new CategoriaCultivo("Frutales", Arrays.asList(
+				repEspecie.getOne((long)1),
+				repEspecie.getOne((long)2)
+				)));
+		repCategoriaCultivo.save(new CategoriaCultivo("Hortalizas", Arrays.asList(
+				repEspecie.getOne((long)3),
+				repEspecie.getOne((long)4),
+				repEspecie.getOne((long)1)
+				)));
+		repCategoriaCultivo.save(new CategoriaCultivo("Cereales", Arrays.asList(
+				repEspecie.getOne((long)5),
+				repEspecie.getOne((long)6),
+				repEspecie.getOne((long)1)
+				)));
+		repCategoriaCultivo.save(new CategoriaCultivo("Oleaginosas", Arrays.asList(
+				repEspecie.getOne((long)7),
+				repEspecie.getOne((long)8)
+				)));
+	}
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<CategoriaCultivo>getCategoriaCultivo(@PathVariable("id") String id){
