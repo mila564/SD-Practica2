eb820a7d9af2f0af1223d5d126bead0fee661252
TntKulix <javinaches@gmail.com>
2021-05-12 13:06:49 +0200

    Añadido constructor de EspecieController

--- src/main/java/sd/urjc/proyecto/controller/EspecieController.java
+++ src/main/java/sd/urjc/proyecto/controller/EspecieController.java
@@ -8,18 +8,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import sd.urjc.proyecto.model.Especie;
 import sd.urjc.proyecto.repository.EspecieRepository;
+import sd.urjc.proyecto.repository.PlagaRepository;
+
+import javax.annotation.PostConstruct;
 
 @Controller
 @RequestMapping("/especies")
 public class EspecieController {
 	@Autowired
 	private EspecieRepository repEspecies;
+	@Autowired
+	private PlagaRepository repPlagas;
 
+	@PostConstruct
+	public void init() {
+		repEspecies.save(new Especie("Manzano", "Malus domestica", Arrays.asList(repPlagas.getOne((long)1), repPlagas.getOne((long)2))));
+		repEspecies.save(new Especie("Peral", "Pyrus communis", Arrays.asList(repPlagas.getOne((long)3), repPlagas.getOne((long)4))));
+		repEspecies.save(new Especie("Naranjo", "Citrus sinensis", Arrays.asList(repPlagas.getOne((long)5), repPlagas.getOne((long)6))));
+		repEspecies.save(new Especie("Fresa", "Fragaria Ananassa", Arrays.asList(repPlagas.getOne((long)7), repPlagas.getOne((long)8))));
+		repEspecies.save(new Especie("Vid", "Vitis vinifera", Arrays.asList(repPlagas.getOne((long)9), repPlagas.getOne((long)10))));
+		repEspecies.save(new Especie("Alcachofa", "Cynara scolymus", Arrays.asList(repPlagas.getOne((long)11), repPlagas.getOne((long)12))));
+		repEspecies.save(new Especie("Lechuga", "Lactuca sativa", Arrays.asList(repPlagas.getOne((long)13), repPlagas.getOne((long)14))));
+		repEspecies.save(new Especie("Cacahuete", "Arachis hypogaea", Arrays.asList(repPlagas.getOne((long)15), repPlagas.getOne((long)16))));
+	}
 	@GetMapping("")
 	public ResponseEntity<List<Especie>> getEspecies() {
 		return new ResponseEntity<>(repEspecies.findAll(), HttpStatus.OK);
