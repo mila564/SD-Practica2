ae5c3959b2a41767033f33cf8b9d9f00327fde65
Milagros Mouriño <milagros2706m@gmail.com>
2021-05-17 21:05:13 +0200

    New changes

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -13,37 +13,40 @@ $(function(){
 				elemento = $('<li>').html(respuesta[categoria].nombre); // Creamos un elemento de la lista por cada categoría existente
 				lista.append(elemento); // Se coloca al final del html
 				lista.append($('</li>'));
+				elemento.addClass(respuesta[categoria].nombre);
 				var button = document.createElement('button'); // Creamos el botón para cada categoría
 				button.type = 'button'; // Indicamos que es de tipo button
 				button.id = respuesta[categoria].nombre;
 				button.innerText = 'Mostrar'; // Le ponemos título al botón
 				elemento.append(button); // Insertamos el botón en el html
-				button.click(desplegarEspecies);
-				elemento.addClass(respuesta[categoria].nombre);
+				$(button).click(desplegarEspecies);
 			}
 		}
 	);
 	function desplegarEspecies(){
-		$(this).innerText = 'Ocultar';
-		url = 'http://localhost:8080/categoriaCultivo/' + $(this).id;
-		var itemLista = $("li[class = $(this).id]");
+		this.innerText = 'Ocultar';
+		url = 'http://localhost:8080/categoriaCultivo/'+ encodeURI(this.id); 
+		var itemLista = $('li.'+this.id);
+		alert(this.id);
 		$.getJSON(url,
 			function(respuesta){
-				itemLista.append($('<h2>').html('Especies')); // Colocamos de título 'Categorías' en el html
-				itemLista.append($('</h2>'));
-				itemLista.append($('<ul id="listaEspecies">')); // Creamos la lista no ordenada de categorías
+				itemLista.append($('<h3>').html('Especies')); // Colocamos de título 'Categorías' en el html
+				itemLista.append($('</h3>'));
+				itemLista.append($('<ul class="' + this.id + '">')); // Creamos la lista no ordenada de categorías
 				itemLista.append($('</ul>'));
-				lista = $('#listaEspecies'); // Obtenemos el elemento html en la variable lista
-				for (especie in respuesta){ // Categoría obtiene el índice de cada una de las categorías obtenidas en el parámetro respuesta que contiene la lista de cultivos
-					elemento = $('<li>').html(respuesta[especie].nombre); // Creamos un elemento de la lista por cada categoría existente
+				lista = $('.'+ this.id); // Obtenemos el elemento html en la variable lista
+				var especiesCultivo = respuesta.especies;
+				for (especie = 0; especie < especiesCultivo.length; especie++){
+					elemento = $('<li>').html(especiesCultivo[especie].nombreVulgar); // Creamos un elemento de la lista por cada categoría existente
 					lista.append(elemento); // Se coloca al final del html
 					lista.append($('</li>'));
 					var button = document.createElement('button'); // Creamos el botón para cada categoría
 					button.type = 'button'; // Indicamos que es de tipo button
-					button.id = respuesta[especie].nombre;
+					button.id = especiesCultivo[especie].nombreVulgar;
 					button.innerText = 'Mostrar'; // Le ponemos título al botón
-					//button.click(desplegarPlagas);
 					elemento.append(button); // Insertamos el botón en el html
+					//$(button).click(desplegarPlagas);
+					elemento.addClass(especiesCultivo[especie].nombreVulgar);					
 				}
 			}
 		);		
